ea7b9f3d69e140d396bee5625d765e714aea3329
Richard <duddydad490@gmail.com>
2024-11-19 20:54:37 +0700

    prisma fix

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    var prisma: PrismaClient;
-  }
-  export let prisma: PrismaClient;
-  if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-  } else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-  }  
+export const prisma = new PrismaClient();
+
+// declare global {
+//     var prisma: PrismaClient;
+//   }
+//   export let prisma: PrismaClient;
+//   if (process.env.NODE_ENV === "production") {
+//     prisma = new PrismaClient();
+//   } else {
+//     if (!global.prisma) {
+//       global.prisma = new PrismaClient();
+//     }
+//     prisma = global.prisma;
+//   }  
 
 
 
